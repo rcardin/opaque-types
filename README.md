f2c339b85c0c4a4e031a0d3bcb9d74aa19c6a4ab
rcardin <riccardo.cardin@gmail.com>
2021-02-14 15:27:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Opaque Types in Scala 3
+
+The repository contains the examples used in the article: [Values Classes In Scala](https://github.com/rcardin/value-classes/blob/master/README.md) 
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,31 @@
+
+object Main {
+
+  object BarCodes {
+    opaque type BarCode = String
+    
+    object BarCode {
+      def mkBarCode(code: String): Either[String, BarCode] = {
+        Either.cond(
+          code.matches("\\d-\\d\\d\\d\\d\\d\\d-\\d\\d\\d\\d\\d\\d"),
+          code,
+          s"The given code $code has not the right format"
+        )   
+      }
+    }
+    
+    extension (b: BarCode) {
+      def country: Char = b.head
+    }
+    
+    val barCode: BarCode = "8-000137-001620"
+  }
+  
+  // Won't compile
+  // val anotherBarCode: BarCode = "8-000137-001620"
+  
+  def main(args: Array[String]): Unit = {
+    val code = BarCodes.BarCode.mkBarCode("8-000137-001620")
+    println(code)
+  }
+}
